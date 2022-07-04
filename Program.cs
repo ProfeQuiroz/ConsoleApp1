c837d8a42013aeda75259dbedeeb50ab81cba005
sp <sp@DESKTOP-DTROK37>
2022-07-04 17:36:44 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int numero1;
+          int numero;
+          int suma; 
+
+            Console.Write ("Ingrese el primer numero:");
+            numero1.Console.Readline
+        }
+    }
+}
